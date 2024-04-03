2a2908a806d4c77ba5093a9368ed6d396796b7be
qdanix43 <159129151+qdanix43@users.noreply.github.com>
2024-04-03 11:09:52 +0200

    commit 1

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Mañanas on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
